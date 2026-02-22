57047246249b16fd05a824927302348999f7aaf9
DONGHOON LEE <donghoonlee@DONGHOONs-MacBook-Pro.local>
2026-02-23 08:58:22 +0900

    test: prettier commit

--- views/home/HomeView.tsx
+++ views/home/HomeView.tsx
@@ -1,9 +1,14 @@
-import styles from '@/views/home/HomeView.module.css';
+import styles from '@/views/home/HomeView.module.css'
+import Nav from '@/components/common/Nav'
+import Header from '@/components/common/Header'
 
 export default function HomeView() {
   return (
     <div>
-      <div className={styles.commonLayoutContainer}></div>
+      <div className={styles.commonLayoutContainer}>
+        <Nav />
+        <Header />
+      </div>
     </div>
-  );
+  )
 }
